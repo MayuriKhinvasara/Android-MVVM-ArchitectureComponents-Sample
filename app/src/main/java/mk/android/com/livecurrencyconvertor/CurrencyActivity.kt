cfb53236e366b71c6f4bb469f0a04468dd9725df
mayurik18 <mayuri.k18@gmail.com>
2018-12-08 18:34:16 +0530

    Initial project setup

---
+++ app/src/main/java/mk/android/com/livecurrencyconvertor/CurrencyActivity.kt
@@ -0,0 +1,12 @@
+package mk.android.com.livecurrencyconvertor
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrencyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_currency)
+    }
+}
